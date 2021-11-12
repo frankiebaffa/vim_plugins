4ac1eb2c3aa4134f00d91c5a3c37535191e5e5d7
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-11-12 13:34:30 -0500

    add scripts for add plugins / language servers

---
+++ add_lang_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# get current directory
+CURR_DIR=$(pwd)
+# get script dir
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+# check args
+if [ -z "$1" ]; then
+	echo "Please include the plugin author's github username as first argument"
+	exit 1
+fi
+if [ -z "$2" ]; then
+	echo "Please include the plugin name as the second argument"
+	exit 1
+fi
+if [ ! -z "$3" ]; then
+	echo "Only two arguments are required"
+	exit 1
+fi
+# add language server plugin
+cd $SCRIPT_DIR &&
+	mkdir -p "$1/start" &&
+	git submodule add "https://github.com/$1/$2" "$1/start/$2" &&
+	echo -ne "# install $2\ncd ./$1/start/$2 && yarn install --frozen-lockfile && cd \$SCRIPT_DIR" &&
+	cd $CURR_DIR
+exit 0
