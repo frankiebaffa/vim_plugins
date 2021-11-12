8845445b94998788b3339bdb84c776b8259031ed
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-11-12 14:09:56 -0500

    improve add_lang_server.sh+improve install.sh

--- add_lang_server.sh
+++ add_lang_server.sh
@@ -16,10 +16,20 @@ if [ ! -z "$3" ]; then
 	echo "Only two arguments are required"
 	exit 1
 fi
+# prepare script to insert
+INSTALL=$(cat << EOM
+if [ "\$1" == "$2" ] || [ "\$1" == "all" ]; then
+	# install $2
+	cd ./$1/start/$2 &&
+		yarn install --frozen-lockfile &&
+		cd \$SCRIPT_DIR
+fi
+EOM
+)
 # add language server plugin
 cd $SCRIPT_DIR &&
 	mkdir -p "$1/start" &&
 	git submodule add "https://github.com/$1/$2" "$1/start/$2" &&
-	echo -ne "# install $2\ncd ./$1/start/$2 && yarn install --frozen-lockfile && cd \$SCRIPT_DIR" &&
+	echo "$INSTALL" >> ./install.sh &&
 	cd $CURR_DIR
 exit 0
--- install.sh
+++ install.sh
@@ -3,26 +3,76 @@
 CURR_DIR=$(pwd)
 # get script dir
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+# check args
+if [ -z "$1" ]; then
+	echo "Please include the name of the lang server to install, if all then include \"all\"";
+	exit 1;
+fi
+# goto script dir
 cd $SCRIPT_DIR
-# install coc-neco
-cd ./neoclide/start/coc-neco && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-rust-analyzer
-cd ./fannheyward/start/coc-rust-analyzer && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-tsserver
-cd ./neoclide/start/coc-tsserver && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-java
-cd ./neoclide/start/coc-tsserver && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-json
-cd ./neoclide/start/coc-json && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-python
-cd ./neoclide/start/coc-python && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-css
-cd ./neoclide/start/coc-css && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-sh
-cd ./josa42/start/coc-sh && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-html
-cd ./neoclide/start/coc-html && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-powershell
-cd ./coc-extensions/start/coc-powershell && yarn install --frozen-lockfile && cd $SCRIPT_DIR
-# install coc-texlab
-cd ./fannheyward/start/coc-texlab && yarn install --frozen-lockfile && cd $SCRIPT_DIR
+if [ "$1" == "coc-neco" ] || [ "$1" == "all" ]; then
+	# install coc-neco
+	cd ./neoclide/start/coc-neco &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-rust-analyzer" ] || [ "$1" == "all" ]; then
+	# install coc-rust-analyzer
+	cd ./fannheyward/start/coc-rust-analyzer &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-tsserver" ] || [ "$1" == "all" ]; then
+	# install coc-tsserver
+	cd ./neoclide/start/coc-tsserver &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-tsserver" ] || [ "$1" == "all" ]; then
+	# install coc-java
+	cd ./neoclide/start/coc-tsserver &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-json" ] || [ "$1" == "all" ]; then
+	# install coc-json
+	cd ./neoclide/start/coc-json &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-python" ] || [ "$1" == "all" ]; then
+	# install coc-python
+	cd ./neoclide/start/coc-python &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-css" ] || [ "$1" == "all" ]; then
+	# install coc-css
+	cd ./neoclide/start/coc-css &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-sh" ] || [ "$1" == "all" ]; then
+	# install coc-sh
+	cd ./josa42/start/coc-sh &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-html" ] || [ "$1" == "all" ]; then
+	# install coc-html
+	cd ./neoclide/start/coc-html &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-powershell" ] || [ "$1" == "all" ]; then
+	# install coc-powershell
+	cd ./coc-extensions/start/coc-powershell &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
+if [ "$1" == "coc-texlab" ] || [ "$1" == "all" ]; then
+	# install coc-texlab
+	cd ./fannheyward/start/coc-texlab &&
+		yarn install --frozen-lockfile &&
+		cd $SCRIPT_DIR
+fi
